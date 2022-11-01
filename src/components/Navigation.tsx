b2dffc0c1697ff96802388d1acfc1d3a6bff791b
MdYousufHossen <yousufadnan111@gmail.com>
2022-11-01 10:56:46 +0600

    add home page link in details page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Menu from "/public/assets/icons/menu.svg";
 import Logo from "/public/assets/icons/react.svg";
 const Navigation = () => {
+    const router=useRouter()
     return (
          <div className="flex justify-between max-w-4xl mx-auto py-5 ">
-          <Image className="clickable" src={Logo} alt=""/>
+          <Image className="clickable" onClick={()=> router.push('/')}src={Logo} alt=""/>
           <div className="flex">
           <h3 className="clickable mr-10 sm:hidden lg:block md:block text-primary ">About us</h3> 
           <h3 className=" mr-10 sm:hidden  lg:block md:block text-primary">What we do</h3> 
